c5de59c0f0c02107c26aa1414f569dfc0676c661
Carlos Carabajal <carjuancara@gmail.com>
2021-10-20 18:58:19 -0300

    finish homework 1,2,3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,6 +3,8 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  let nombres = ['JUAN', 'PEDRO', 'PABLO', 'JOAGUIN'];
+  return nombres[array];  
 }
 
 
